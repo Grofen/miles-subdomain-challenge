59a3ba8a8fd46dc6a2aa105d777aa56d9a8975c7
Mohammad Ghorab <mody.moustafa@yahoo.com>
2023-09-19 22:28:04 +0200

    refactor hom and profile pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import Page from "components/shared/page";
 import Home from "../../components/home";
 
 const HomePage = (): JSX.Element => {
-  return <Home />;
+  return (
+    <Page name="home" title="Home">
+      <Home />
+    </Page>
+  );
 };
 
 export default HomePage;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,28 @@
-import Profile from "../../components/profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { UnexpectedError } from "components/shared/error";
+import Loading from "components/shared/loading";
+import Page from "components/shared/page";
+import Profile from "components/profile";
 
 const ProfilePage = (): JSX.Element => {
-  return <Profile />;
+  const { user, isLoading, error } = useAuth0();
+
+  let content;
+
+  if (isLoading) {
+    content = <Loading />;
+  } else if (error) {
+    content = <UnexpectedError />;
+  } else if (user) {
+    content = <Profile user={user} />;
+  }
+
+  return (
+    <Page name="profile" title="Profile">
+      {content}
+    </Page>
+  );
 };
 
 export default ProfilePage;
