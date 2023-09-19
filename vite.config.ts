74716216a5bceffefca4efaef2541795fbe3f916
Mohammad Ghorab <mody.moustafa@yahoo.com>
2023-09-19 22:26:27 +0200

    config vitest jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
@@ -5,4 +6,19 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.test.tsx"],
+    setupFiles: "./vitest-setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+      components: "/src/components",
+      config: "/src/config",
+      images: "/src/images",
+      "test-utils": "/src/test-utils",
+    },
+  },
 });
